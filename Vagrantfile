1b3fa4924a754e69c874d8a42bdb6d8fd59831f7
Johan Sydseter <johan.sydseter@admincontrol.com>
2022-05-20 11:54:41 +0200

    Do not upgrade pip, use apt package manager for installation of mycli. upgrade nodejs from 6 to 10 and use zst for unpacking unison installation

--- Vagrantfile
+++ Vagrantfile
@@ -6,6 +6,8 @@ VAGRANTFILE_API_VERSION = "2"
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = "ubuntu/xenial64"
   config.vm.network "private_network", ip: "10.10.10.5"
+  config.vm.network "forwarded_port", guest: 80, host: 8090
+  config.vm.network "forwarded_port", guest: 443, host: 8443
   config.vm.hostname = "FacebookCTF-Dev"
   config.ssh.shell = "bash -c 'BASH_ENV=/etc/profile exec bash'"
   config.vm.provision "shell", path: "extra/provision.sh", args: ENV['FBCTF_PROVISION_ARGS'], privileged: false
--- extra/provision.sh
+++ extra/provision.sh
@@ -295,11 +295,9 @@ fi
             package python-all-dev
             package python-setuptools
             package python-pip
-            log "Upgrading pip"
-            sudo -H pip install --upgrade pip
-            log "Installing pip - mycli"
-            sudo -H pip install mycli
-            package emacs
+            log "Installing mycli"
+            package mycli
+	    package emacs
             package htop
         fi
 
@@ -320,6 +318,7 @@ fi
 
         log "Installing unison 2.48.3. Remember to install the same version on your host machine"
         package xz-utils
+        package zstd
         install_unison
     fi
 
